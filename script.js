c72da9d610b491c5d839c8d55c6fa0e028cd1d27
khushboo <khushbookhushboo20093@gmail.com>
2024-12-23 09:10:32 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function highlight() {
     //Write your code here
-
-
+    var boldItems = document.getElementsByTagName("strong");
+    for (var i = 0; i < boldItems.length; i++) {
+        boldItems[i].style.color = "green";
+    }
 }
-
-
 function return_normal() {
     //Write your code here
 
-    
+    var boldItems = document.getElementsByTagName("strong");
+    for (var i = 0; i < boldItems.length; i++) {
+        boldItems[i].style.color = "black";
+    }
 }
